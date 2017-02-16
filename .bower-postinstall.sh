5c0f8a4d7a42b3751bc5c120be3cafeae7e67107
wandypurnomo <wandy@mediatechindo.com>
2017-02-16 15:26:49 +0700

    init project

---
+++ .bower-postinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## cp bootstrap font
+cp bower_components/bootstrap-sass/assets/fonts/bootstrap/* assets/fonts
+
+## cp bootstrap js
+cp bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js assets/js
+
+## cp jquery
+cp bower_components/jquery/dist/jquery.min.js assets/js
